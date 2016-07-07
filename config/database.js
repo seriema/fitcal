3b6e0a09e18114552b8e1955896b040229b4a8b2
John-Philip Johansson <seriema@gmail.com>
2016-07-07 09:12:04 +0200

    Rewrite: basic site with local login
    Based on:
    https://scotch.io/tutorials/easy-node-authentication-setup-and-local
    
    https://scotch.io/tutorials/upgrading-our-easy-node-authentication-serie
    s-to-expressjs-4-0

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+//  url: 'mongodb://fitcal:q757WKd5tHFwtTfBNIBvhJpzbYEcV93Nu0SN7GzfbzIO6ujJ1kP50qgRHFbvw3z34hEj5TUfIyJuWcISCoKVJg==@fitcal.documents.azure.com:10250/?ssl=true'
+  url: process.env.LIFECAL_MONGODB_URL
+};
