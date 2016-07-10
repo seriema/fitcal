5ae4b67a791a996b7df89973e27333fe2ad9a123
John-Philip Johansson <seriema@gmail.com>
2016-07-10 19:09:47 +0200

    Bunch all categories together in one save call
    Trying to avoid hitting the Azure request limit.

--- app/api/fitbit.js
+++ app/api/fitbit.js
@@ -22,95 +22,108 @@ const timeSpan = {
     period: '7d'
 };
 
+function saveSleepDate(userFitbitId, date, data) {
+    let categoryDeferred = q.defer();
+    const query = {
+        'fitbit.id': userFitbitId,
+        dateTime: date
+    };
+
+    Sleep.findOne(query, function (err, sleep) {
+
+        // if there is an error, stop everything and return that
+        // i.e. an error connecting to the database
+        if (err)
+            return categoryDeferred.reject(err);
+
+        // if the sleep data is not found, create one
+        if (!sleep) {
+            sleep = new Sleep();
+            sleep.fitbit.id = userFitbitId;
+            sleep.dateTime = date;
+        }
+
+        // add the data we went through all this trouble to get
+        Object.keys(data).forEach(category => {
+            sleep.raw[category] = data[category];
+        });
+
+        // save the sleep data to the database
+        sleep.save(function (err) {
+            if (err)
+                return categoryDeferred.reject(err);
+
+            categoryDeferred.resolve();
+        });
+    });
+
+    return categoryDeferred.promise;
+}
+
+function saveTimeSeries(userFitbitId, userData) {
+    return Object.keys(userData).map(date => {
+        return saveSleepDate(userFitbitId, date, userData[date])
+    });
+}
+
+// TODO: handle various Fitbit return results, like token expired, etc.
+function callFitbit(token, category) {
+    const scope = 'sleep';
+    var path = `/${scope}/${category}/date/${timeSpan.baseDate}/${timeSpan.period}.json`;
+    return client.get(path, token).then(function (result) {
+        var data = result[0];
+        if (data.success === false) {
+            let error = data.errors[0];
+            if (error.errorType === 'expired_token') {
+                throw error; // TODO: Renew the token or something.
+            } else {
+                throw error; // What other errors can I get?
+            }
+        }
+
+        return {
+            scope,
+            category,
+            data: data[`sleep-${category}`]
+        };
+    });
+}
+
+function joinTimeSeries(timeSeries) {
+    let dateObjects = {};
+
+    timeSeries.forEach( time => {
+        time.data.forEach( day => {
+            if (!dateObjects[day.dateTime]) {
+                dateObjects[day.dateTime] = {};
+            }
+
+            dateObjects[day.dateTime][time.category] = day.value;
+        });
+    });
+
+    return dateObjects;
+}
+
 function importSleep(user, res) {
     function updateCategory(categoryData) {
         function updateCategoryDate(category, dateData) {
-            var categoryDeferred = q.defer();
             var query = {
                 'fitbit.id': user.fitbit.id,
                 dateTime: dateData.dateTime
             };
-            /* UPDATE version, doesn't seem to work. It overwrites.
-             var queryOptions = {
-             upsert: true // create if it doesn't exist
-             };
-
-             var docUpdate = { raw: {} };
-             docUpdate.raw[categoryData.category] = data.value;
-
-             Sleep.update(query, docUpdate, queryOptions, function (err) {
-             if (err)
-             return categoryDeferred.reject(err);
-
-             categoryDeferred.resolve();
-             });
-             */
-
-            Sleep.findOne(query, function (err, sleep) {
-
-                // if there is an error, stop everything and return that
-                // i.e. an error connecting to the database
-                if (err)
-                    return categoryDeferred.reject(err);
-
-                // if the sleep data is not found, create one
-                if (!sleep) {
-                    sleep = new Sleep();
-                    sleep.fitbit.id = user.fitbit.id;
-                    sleep.dateTime = dateData.dateTime;
-                }
-
-                // add the data we went through all this trouble to get
-                sleep.raw[category] = dateData.value;
-
-                // save the sleep data to the database
-                sleep.save(function (err) {
-                    if (err)
-                        return categoryDeferred.reject(err);
-
-                    categoryDeferred.resolve();
-                });
-            });
-
-            return categoryDeferred.promise;
+            return saveSleepDate(query, user, dateData, category);
         }
 
         var categoryPromises = categoryData.data.map(updateCategoryDate.bind(null, categoryData.category));
         return q.all(categoryPromises);
     }
 
-    var fitbitPromises = resources.sleep.map(function (category) {
-        var path = `/sleep/${category}/date/${timeSpan.baseDate}/${timeSpan.period}.json`;
-        return client.get(path, user.fitbit.token).then(function (result) {
-            var data = result[0];
-            if (data.success === false) {
-                let error = data.errors[0];
-                if (error.errorType === 'expired_token') {
-                    throw error; // TODO: Renew the token or something.
-                } else {
-                    throw error; // What other errors can I get?
-                }
-            }
+    var fitbitPromises = resources.sleep.map(callFitbit.bind(null, user.fitbit.token));
 
-            return {
-                category: category,
-                data: data[`sleep-${category}`]
-            };
-        });
-    });
     q.all(fitbitPromises)
-        .then(function (categoryResults) {
-            // We do this in a sequence to avoid race conditions when updating the database,
-            // where it could result in multiple mongoose documents.
-            var funcs = categoryResults.map(function (categoryResult) {
-                return updateCategory.bind(null, categoryResult);
-            });
-            var result = q(0);
-            funcs.forEach(function (f) {
-                result = result.then(f);
-            });
-            return result;
-        })
+        .then(joinTimeSeries)
+        .then(saveTimeSeries.bind(null, user.fitbit.id))
         .then(function () {
             res.send(`Import complete of ${resources.sleep.length} sleep categories from ${timeSpan.baseDate} and going back ${timeSpan.period}.`);
         })
